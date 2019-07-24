0c34941260d1db0584fa5c999ddde2b3593ccb15
Oleksandr Ignatenko <avi.cpp@gmail.com>
2019-07-24 17:34:10 +0200

    Add create user (POST /users) endpoint with tests

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,5 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Resource, Api
+from sqlalchemy import exc
+
+from project import db
+from project.api.models import User
 
 blueprint = Blueprint('users', __name__)
 api = Api(blueprint)
@@ -11,5 +15,94 @@ class UsersPing(Resource):
             'message': 'pong'
         }
 
+class UsersList(Resource):
+    def post(self):
+        data = request.get_json()
+
+        email = data.get('email')
+        username = data.get('username')
+
+        if not email:
+            return {
+                'message': 'Invalid payload. \'email\' not given',
+                'status': 'fail',
+            }, 400
+
+        if not username:
+            return {
+                'message': 'Invalid payload. \'username\' not given',
+                'status': 'fail',
+            }, 400
+
+        user_by_email = User.query.filter_by(email=email).first()
+        user_by_username = User.query.filter_by(username=username).first()
+
+        if not user_by_email and not user_by_username:
+            try:
+                user = User(username=username, email=email)
+                db.session.add(user)
+                db.session.commit()
+
+                return {
+                    'message': f'User {user.username} with email "{user.email}" has been created',
+                    'status': 'success'
+                }, 201
+            except exc.IntegrityError:
+                db.session.rollback()
+                return {
+                    'message': 'Invalid payload',
+                    'status': 'fail'
+                }, 400
+        elif user_by_email:
+            return {
+                'message': f'Invalid payload. User with email "{email}" already exists',
+                'status': 'fail'
+            }, 400
+        elif user_by_username:
+            return {
+                'message': f'Invalid payload. User with username "{username}" already exists',
+                'status': 'fail'
+            }, 400
+
+
+
+#        post_data = request.get_json()
+#        email = post_data.get('email')
+#        username = post_data.get('username') or email
+#        user = User(username=username, email=email)
+#
+#        if not email:
+#            response_data = {
+#                'message': 'Invalid payload. No "email" key given',
+#                'status': 'fail',
+#            }
+#            return response_data, 400
+#        
+#        try:
+#            user = User.query.filter_by(email=email).first()
+#            if not user:
+#                user = User(username=username, email=email)
+#                db.session.add(user)
+#                db.session.commit()
+#                response_data = {
+#                    'status': 'success',
+#                    'message': f'{email} has been added'
+#                }
+#                return response_data, 201
+#            else:
+#                response_data = {
+#                    'message': 'Invalid email. Given email already exists.',
+#                    'status': 'fail',
+#                }
+#                return response_data, 400
+#        except exc.IntegrityError:
+#            db.session.rollback()
+#            response_data = {
+#                'message': 'Invalid username. Given username already exists.',
+#                'status': 'fail',
+#            }
+#            return response_data, 400
+
 api.add_resource(UsersPing, '/users/ping')
+api.add_resource(UsersList, '/users')
 
--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -7,14 +7,145 @@ class TestUserService(BaseTestCase):
     """ Tests for `users` Service """
 
     def test_users(self):
-        """ /users/ping returns correct response """
+        """ GET /users/ping returns correct response """
 
-        response = self.client.get('/users/ping')
-        data = json.loads(response.data.decode())
+        with self.client:
+            response = self.client.get('/users/ping')
+            data = json.loads(response.data.decode())
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('pong', data['message'])
-        self.assertIn('success', data['status'])
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('pong', data['message'])
+            self.assertIn('success', data['status'])
+
+    def test_add_user(self):
+        """ POST /users creates new user """
+        
+        username = 'joe'
+        email = 'joe.doe@example.com'
+        data = json.dumps({
+            'username': username,
+            'email': email
+        })
+
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=data,
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn(username, data['message'])
+            self.assertIn(email, data['message'])
+            self.assertIn('success', data['status'])
+
+    def test_add_user_without_username(self):
+        """ POST /users without username returns an error """
+
+        email = 'joe.doe@example.com'
+
+        data = json.dumps({'email': email})
+
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=data,
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload', data['message'])
+            self.assertIn('username', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_without_email(self):
+        """ POST /users without 'email' key returns an error """
+
+        username = 'joe'
+
+        data = json.dumps({'username': username})
+
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=data,
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload', data['message'])
+            self.assertIn('email', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_duplicate_email(self):
+        """ POST /users returns an error if given email already exists """
+
+        username1 = 'joe'
+        username2 = 'jane'
+        email = 'joe.doe@example.com'
+
+        data1 = json.dumps({
+            'username': username1,
+            'email': email
+        })
+
+        data2 =  json.dumps({
+            'username': username2,
+            'email': email
+        })
+
+        with self.client:
+            self.client.post(
+                '/users',
+                data=data1,
+                content_type='application/json',
+            )
+            response = self.client.post(
+                '/users',
+                data=data2,
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload', data['message'])
+            self.assertIn('email', data['message'])
+            self.assertIn('exists', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_duplicate_username(self):
+        """ POST /users returns an error if given username already exists """
+
+        username = 'joe'
+        email1 = 'joe.doe@example.com'
+        email2 = 'jane.doe@example.com'
+
+        data1 = json.dumps({
+            'username': username,
+            'email': email1
+        })
+
+        data2 =  json.dumps({
+            'username': username,
+            'email': email2
+        })
+
+        with self.client:
+            self.client.post(
+                '/users',
+                data=data1,
+                content_type='application/json',
+            )
+            response = self.client.post(
+                '/users',
+                data=data2,
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload', data['message'])
+            self.assertIn('username', data['message'])
+            self.assertIn('exists', data['message'])
+            self.assertIn('fail', data['status'])
 
 if __name__ == '__main__':
     unittest.main()
