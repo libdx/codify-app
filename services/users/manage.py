aa45ad1fc32acc4061e2939ebe31a69b2d2fdf54
Oleksandr Ignatenko <avi.cpp@gmail.com>
2019-07-26 17:06:42 +0200

    Setup code coverage for tests

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,21 +2,44 @@
 
 import sys
 import unittest
+import coverage
+import click
 
 from flask.cli import FlaskGroup
 
 from project import create_app, db
 from project.api.models import User
 
+def start_coverage():
+    cov = coverage.coverage(
+        branch=True,
+        include='project/*',
+        omit=[
+            'project/tests/*',
+            'project/config.py',
+        ]
+    )
+    cov.start()
+    return cov
+
 cli = FlaskGroup(create_app=create_app)
 
 @cli.command()
-def test():
-    """ Runs the tests without code coverage """
+@click.option('--coverage', '-c', is_flag=True)
+def test(coverage):
+    """Runs the tests optionally with code coverage"""
+    cov = start_coverage()
 
     tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
+        if coverage:
+            cov.stop()
+            cov.save()
+            print('\nCoverage Summary:')
+            cov.report()
+            cov.html_report()
+            cov.erase()
         return 0
     sys.exit(result)
 
